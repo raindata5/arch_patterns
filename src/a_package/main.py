a37b0c5f7d672c16717b9779228077eb286191de
Ronald <ronalddavisr3@gmail.com>
2023-01-09 21:55:16 -0500

    add allocate batch endpoint

--- src/a_package/main.py
+++ src/a_package/main.py
@@ -1,11 +1,20 @@
-from typing import Union
+from typing import (
+    Union,
+    Any
+)
 from fastapi import FastAPI
 from a_package.orm import Session
 import a_package.repository as repository
 import a_package.model as model
+from a_package.utils import (
+    allocate_batch
+)
 import datetime as dt
 from sqlalchemy import select
-
+from pydantic import BaseModel
+1
+class OrderReference(BaseModel):
+    order_reference : str
 
 sql_repo = repository.SqlRepository(Session())
 app = FastAPI()
@@ -20,8 +29,13 @@ def read_batch(batch_reference: str):
     return queried_batch
     
 @app.post("/allocate")
-def allocate_batch():
-    sql_repo
+def allocate_batch_ep(order_reference: OrderReference):
+    queried_order: Union[model.Order, Any] = sql_repo.get(model.Order, model.Order.order_reference, order_reference.order_reference)
+    
+    queried_batches = sql_repo.list(model.Batch, model.Batch.sku, queried_order.order_lines[0].sku)
+    queried_batches_list = list(queried_batches)
+    best_batch = allocate_batch(queried_order, queried_batches_list)
+    return best_batch
 
 # from enum import IntEnum
 # class ShippingState(IntEnum):
