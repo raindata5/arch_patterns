42784a41798736ec1a11d8f158280922143981d9
Ronald <ronalddavisr3@gmail.com>
2023-01-13 21:57:34 -0500

    refactor allocate endpoint with exception handling;make respective tests

--- src/a_package/main.py
+++ src/a_package/main.py
@@ -1,8 +1,13 @@
+import logging
 from typing import (
     Union,
     Any
 )
-from fastapi import FastAPI
+from fastapi import (
+    FastAPI,
+    status,
+    HTTPException
+)
 from a_package.orm import Session
 import a_package.repository as repository
 import a_package.model as model
@@ -14,8 +19,14 @@ from sqlalchemy import select
 from pydantic import BaseModel
 1
 class OrderReference(BaseModel):
-    order_reference : str
+    order_reference: str
 
+class Sku(BaseModel):
+    sku: str
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 sql_repo = repository.SqlRepository(Session())
 app = FastAPI()
 
@@ -26,15 +37,32 @@ def read_root():
 @app.get("/batch/{batch_reference}")
 def read_batch(batch_reference: str):
     queried_batch = sql_repo.get(model.Batch, model.Batch.reference, batch_reference)
+    sql_repo.commit()
     return queried_batch
     
-@app.post("/allocate")
-def allocate_batch_ep(order_reference: OrderReference):
-    queried_order: Union[model.Order, Any] = sql_repo.get(model.Order, model.Order.order_reference, order_reference.order_reference)
-    
-    queried_batches = sql_repo.list(model.Batch, model.Batch.sku, queried_order.order_lines[0].sku)
-    queried_batches_list = list(queried_batches)
-    best_batch = allocate_batch(queried_order, queried_batches_list)
+@app.post("/allocate", status_code=status.HTTP_201_CREATED, )
+def allocate_batch_ep(order_reference: OrderReference, sku: Sku):
+    #TODO: Allow the client to specify a sku
+    try:
+        queried_order: Union[model.Order, Any] = sql_repo.get(model.Order, model.Order.order_reference, order_reference.order_reference)
+        if not queried_order:
+            raise HTTPException(status.HTTP_404_NOT_FOUND, detail=F"No order found with the following order_reference {order_reference}")
+        logging.info(queried_order)
+        queried_order_line = queried_order.search_order_line(sku.sku)
+        sku_order_line = queried_order_line.sku
+        logging.info(sku_order_line)
+        queried_batches = sql_repo.list(model.Batch, model.Batch.sku, sku_order_line)
+        queried_batches_list = list(queried_batches)
+        if len(queried_batches_list) == 0:
+            raise HTTPException(status.HTTP_404_NOT_FOUND, detail=F"No batch found with the following {sku_order_line}")
+        logging.info(queried_batches_list) # run tests twice and print skus
+        best_batch = allocate_batch(queried_order, queried_batches_list)
+    except HTTPException as ex:    
+        raise
+    except ValueError as ex:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail=F"order:{queried_order.order_reference} not containing an order_line with the following sku: {sku.sku}")
+    finally:
+        sql_repo.commit()
     return best_batch
 
 # from enum import IntEnum
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,44 @@
 from a_package import model
 import datetime as dt
-from a_package.orm import Session
 import a_package.repository as repository
 import pytest
 from a_package import utils
+import logging
+import psycopg2
+from sqlalchemy.orm import sessionmaker
+from a_package import model
+from sqlalchemy import create_engine
+import datetime as dt
+from a_package import utils
+from a_package.config import settings
+from a_package.orm import Session
 
-@pytest.fixture
+engine = create_engine(f"postgresql://{settings.pg_oltp_api_user}:{settings.pg_oltp_api_password}@{settings.pg_oltp_api_host}:{settings.pg_oltp_api_port}", echo=True)
+
+print(f"postgresql://{settings.pg_oltp_api_user}:{settings.pg_oltp_api_password}@{settings.pg_oltp_api_host}:{settings.pg_oltp_api_port}")
+Session = sessionmaker(bind=engine, expire_on_commit=False)
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+@pytest.fixture()
 def get_sql_repo():
-    sql_repo = repository.SqlRepository(Session())
-    return sql_repo
+    with open("/architecture_patterns/postgres-table-truncates.sql") as sql_file: 
+        ps_conn = psycopg2.connect("postgresql://postgres:example@postgres:5432",)
+        ps_cursor = ps_conn.cursor()
 
-@pytest.fixture
-def return_addi_sample_data(get_sql_repo):
+        ps_conn.commit()
+        
+        ps_cursor.execute(sql_file.read())
+
+        ps_conn.commit()
+        ps_cursor.close()
+        ps_conn.close()
+
+
+@pytest.fixture()
+def return_base_sample_data(get_sql_repo):
     with Session() as session:
         sku_ref_natty, sku_ref_natty_01 = utils.random_sku("NaTTY"), utils.random_sku("NaTTY_01")
         batch_ref_nat = utils.random_batchref("NaT")
@@ -23,13 +50,25 @@ def return_addi_sample_data(get_sql_repo):
             dict(sku=sku_ref_natty, quantity=10),
             dict(sku=sku_ref_natty_01, quantity=1)
     ]
-        [order_nat.attach_order_line(ol) for ol in order_lines_params]
+        list_ol = [order_nat.attach_order_line(ol) for ol in order_lines_params]
         # new_batch.allocate_stock(ex_order)
-        session.add_all([batch_nat, order_nat,])
-    #     stmt = select(model.Batch).where(model.Batch.reference == 'TKJ-23244')
-    #     patrick = session.scalars(stmt).one()
-    #     patrick: model.Batch
-    #     print(vars(patrick))
-    #     print(patrick.orders)
-    #     session.commit()
-    batch_nat, order_nat, order_lines_params
+        session.add_all([batch_nat, order_nat,])  
+        session.commit()
+    return batch_nat, order_nat, list_ol
+
+@pytest.fixture
+def return_serialized_order(get_sql_repo):
+    with Session() as session:
+        sku_ref_natty, sku_ref_natty_01 = utils.random_sku("NaTTY"), utils.random_sku("NaTTY_01")
+
+        order_ref_nat = utils.random_orderid("nat_order")
+
+        order_nat = model.Order(order_reference=order_ref_nat)
+        order_lines_params = [
+            dict(sku=sku_ref_natty, quantity=10),
+            dict(sku=sku_ref_natty_01, quantity=1)
+    ]
+        list_ol = [order_nat.attach_order_line(ol) for ol in order_lines_params]
+        session.add_all([order_nat,])  
+        session.commit()
+    return order_nat, list_ol
